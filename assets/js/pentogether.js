9b81f53099879933b37c63fa2072d78f0858b873
wyh <wuyuanhang54321@gmail.com>
2018-04-06 14:54:21 +0800

    人人对喷界面v1.0

--- assets/js/pentogether.js
+++ assets/js/pentogether.js
@@ -1,5 +1,26 @@
 
 $(function(){
+    //建立websocket链接
+    var socket = new WebSocket("ws://localhost:8080/chat/12345");
+    //监听是否有消息送达
+    socket.addEventListener('message',function(event){
+         var message=event.data
+        var html= '<div class="row">\
+            <div class="col-md-12 rightname">\
+            <p>啦啦啦</p>\
+            </div>\
+            </div>\
+            <div class="row">\
+            <div class="col-md-1 " >\
+            <img alt="" src="../assets/images/test.jpg" class="pictureleft"/>\
+            </div>\
+            <div class="col-md-11 " >\
+            <p class="itemleft">'+dsssssssddddddddddddddddddd+'</p>\
+            </p>\
+            </div>\
+            </div>'
+        $("#chatiframe").append(html);
+    })
     //发送
  $("#send").click(function(){
      var message=$("#sendtext").val()
@@ -13,9 +34,10 @@ $(function(){
          <p class="itemright">'+message+'</p>'
          '</div>\
          <div class="col-md-1 " >\
-         <img alt="" src="./images/icon_weixin.png" class="pictureright"/>\
+         <img alt="" src="../images/test.jpg" class="pictureright"/>\
          </div>\
          </div>'
+     socket.send(message)   //将消息发送至服务器
      $("#chatiframe").append(html);
      $("#sendtext").val("")
  })
